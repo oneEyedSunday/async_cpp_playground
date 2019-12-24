fdc103f4d706db4aedde32a104654e36220294f8
Idiakose O. Sunday <idiakosesunday@gmail.com>
2019-12-24 02:21:40 +0100

    Initial Commit

---
+++ async/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  async
+//
+//  Created by Sunday Idiakose on 24/12/2019.
+//  Copyright © 2019 Sunday Idiakose. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
